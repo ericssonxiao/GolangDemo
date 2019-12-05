3df7c363053c1a741d358b710bf424241b97ef14
ericssonxiao <ericssonxiao@gmail.com>
2019-12-05 15:45:34 -0500

    study

---
+++ 1.BasicGolang/ex01.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func tryOpenFile() {
+	f, err := os.OpenFile("notes.txt", os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	fmt.Println("this is simple function, hello world.")
+	//tryOpenFile()
+
+	stringMap := map[int]string{1: "A", 2: "B", 3: "C"}
+	for k, v := range stringMap {
+		fmt.Printf("%d: %s\n", k, v)
+	}
+}
---
+++ ex02.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+//array
+func myArray() {
+	arr := []int{1, 2, 3, 4, 5}
+	//arr2 := []int{5, 4, 3, 2, 1}
+	fmt.Println(arr)
+}
+
+func main() {
+	fmt.Printf("hello %s\n", "world")
+	m1 := 1
+	m2 := 2
+	fmt.Println(int16(m1 + m2))
+
+	ss := "This is a test"
+	fmt.Println(ss)
+
+	myArray()
+}
